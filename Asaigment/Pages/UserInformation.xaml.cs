e11380448c728359cf1021910ff869dd06f44895
lebach2000 <kenrou.hol0.the.wolf@gmail.com>
2019-11-06 15:18:37 +0700

    asigment

---
+++ Asaigment/Pages/UserInformation.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using T1808AHelloUWP.Entity;
+using T1808AHelloUWP.Service;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace T1808AHelloUWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UserInformation : Page
+    {
+        private IMemberService _memberService;
+        private IFileService _fileService;
+
+        public UserInformation()
+        {
+            Debug.WriteLine("Init userinformation.");
+            this.InitializeComponent();
+            this._memberService = new MemberService();
+            this._fileService = new LocalFileService();
+            this.Loaded += LoadUserInformation;
+        }
+
+        private async void LoadUserInformation(object sender, RoutedEventArgs e)
+        {
+            MemberCredential memberCredential = ProjectConfiguration.CurrentMemberCredential;
+            if (ProjectConfiguration.CurrentMemberCredential == null)
+            {
+                memberCredential = await this._fileService.ReadMemberCredentialFromFile();
+            }
+            if (memberCredential == null)
+            {
+                this.Frame.Navigate(typeof(LoginPage));
+            }
+
+            if (memberCredential != null)
+            {
+                var member = this._memberService.GetInformation(memberCredential.token);
+                Email.Text = member.email;
+                Name.Text = member.firstName + " " + member.lastName;
+            }
+        }
+    }
+}
